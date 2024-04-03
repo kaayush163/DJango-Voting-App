32936a0787f1241cd17520008801acdea405187a
kaayush163 <kaayush163>
2024-04-04 01:51:53 +0530

    setup virtual env and views function logic

---
+++ pollapp/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+from django.http import JsonResponse,HttpResponse
+# Create your views here.
+arr = ['Java','Python','Cplusplus','C','DotNet','Javascript','PHP','Swift']
+globalcnt = dict()
+
+def index(request):
+    # return HttpResponse("server started")
+    mydict = {
+       "arr":arr 
+    }
+    return render(request,'index.html',context=mydict)
+
+def getquery(request):
+    q = request.GET['languages'] #languages come from the name of index.html form get
+    if q in globalcnt:
+        globalcnt[q] = globalcnt[q] + 1
+    else:
+        globalcnt[q]=1  #this is for voting count logic
+    mydict = {
+        "arr" : arr,
+        "globalcnt":globalcnt
+    }
+    return render(request,'index.html',context = mydict)
